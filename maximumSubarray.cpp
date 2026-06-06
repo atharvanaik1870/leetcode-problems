94f6bab028a533609d2ab4790c0e0a54de1087cf
atharvanaik1870 <55330511+atharvanaik1870@users.noreply.github.com>
2026-06-06 15:18:14 +0530

    Create maximumSubarray.cpp

---
+++ maximumSubarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            currentSum = std::max(nums[i], currentSum + nums[i]);
+            maxSum = std::max(maxSum, currentSum);
+        }
+        return maxSum;
+    }
+};
