119f266731100df0838e8c98de5f19ab90df1fd3
atharvanaik1870 <55330511+atharvanaik1870@users.noreply.github.com>
2026-06-06 15:21:33 +0530

    Update maximumSubarray.cpp

--- maximumSubarray.cpp
+++ maximumSubarray.cpp
@@ -1,3 +1,4 @@
+//This is Kedane's algo woth O(n) time complexity.
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
@@ -7,6 +8,27 @@ public:
         for (int i = 1; i < nums.size(); i++) {
             currentSum = std::max(nums[i], currentSum + nums[i]);
             maxSum = std::max(maxSum, currentSum);
+        }
+        return maxSum;
+    }
+};
+
+// Approach 2: Brute force with O(n^2) time complexity maybe not best solution but still works
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = nums[0];
+        for (int i = 0; i < nums.size(); i++) {
+            int currSum = nums[i];
+            for (int j = i + 1; j < nums.size(); j++) {
+                currSum = currSum + nums[j];
+                if (currSum > maxSum) {
+                    maxSum = currSum;
+                }
+            }
+            if (currSum > maxSum) {
+                maxSum = currSum;
+            }
         }
         return maxSum;
     }
