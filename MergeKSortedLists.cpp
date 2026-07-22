73fa1287c60c9b0d6704857094ac79d47696009c
atharvanaik1870 <55330511+atharvanaik1870@users.noreply.github.com>
2026-07-22 16:54:57 +0530

    Update MergeKSortedLists.cpp

--- MergeKSortedLists.cpp
+++ MergeKSortedLists.cpp
@@ -30,3 +30,43 @@ public:
         return dummy->next;
     }
 };
+//Approach 2: using minimum heap
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>,
+                       greater<pair<int, ListNode*>>>
+            pq;
+        for (int i = 0; i < lists.size(); i++) {
+            if (lists[i]) {
+                pq.push({lists[i]->val, lists[i]});
+            }
+        }
+
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+
+        while (!pq.empty()) {
+            auto p = pq.top();
+            pq.pop();
+
+            if (p.second->next) {
+                pq.push({p.second->next->val, p.second->next});
+            }
+            //why second?? -> cox first has the value and 2ns is node
+            temp->next = p.second;
+            temp = temp->next;
+        }
+        return dummy->next;
+    }
+};
