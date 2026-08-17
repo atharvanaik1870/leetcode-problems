0b46266e35525620ca0c63134e1e62d20d0cc939
atharvanaik1870 <55330511+atharvanaik1870@users.noreply.github.com>
2026-08-17 16:00:05 +0530

    Update TrappingRainWater.cpp

--- TrappingRainWater.cpp
+++ TrappingRainWater.cpp
@@ -25,3 +25,23 @@ public:
         return ans;
     }
 };
+//Approach 2:
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int l = 0, r = n-1, leftMax = 0, rightMax = 0, total = 0;
+
+        while(l < r){
+            leftMax = max(leftMax, height[l]);
+            rightMax = max(rightMax, height[r]);
+
+            if(leftMax <= rightMax)
+                total += (leftMax - height[l++]);
+            else
+                total += (rightMax - height[r--]);
+        }
+
+        return total;
+    }
+};
