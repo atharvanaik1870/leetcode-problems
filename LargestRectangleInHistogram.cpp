a9649699b03ce9e9178412b592e7b57f6860109c
atharvanaik1870 <55330511+atharvanaik1870@users.noreply.github.com>
2026-08-27 15:22:40 +0530

    Create LargestRectangleInHistogram.cpp

---
+++ LargestRectangleInHistogram.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> left(n, 0);
+        vector<int> right(n, 0);
+
+        stack<int> st;
+
+        // first find previous smallest element
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && heights[st.top()] >= heights[i])
+                st.pop();
+
+            left[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+
+        while (!st.empty()) {
+            st.pop();
+        }
+
+        // 2nd find next smallest element
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && heights[st.top()] >= heights[i])
+                st.pop();
+
+            right[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int currArea = heights[i] * (right[i] - left[i] - 1);
+
+            ans = std::max(ans, currArea);
+        }
+        return ans;
+    }
+};
